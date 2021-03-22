06245d7396eb617ddde142274a9eb4cc1810a64b
diyorbekdev <diyorbektoraboyev782@gmail.com>
2021-03-22 21:05:21 +0500

    script.js fayli o'zgardi

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const menuBtn = document.querySelector('.menu-btn');
+        let menuOpen = false;
+        menuBtn.addEventListener('click', () => {
+            if (!menuOpen) {
+                menuBtn.classList.add('open');
+                document.getElementById('menu').style.left = "0%"
+                menuOpen = true;
+            } else {
+                menuBtn.classList.remove('open');
+                menuOpen = false;
+                document.getElementById('menu').style.left = "-100%"
+            }
+
+        });
+
+        function addAnchor() {
+            document.getElementById('menu').style.left = "-100%"
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+            $('.navbarMe').css({
+                "display": "flex",
+            })
+        }
+        $(window).on('load', function () {
+
+            document.getElementById('all').style.display = "block"
+            document.getElementById('loader').style.display = "none"
+
+            AOS.init();
+
+            $('.li').click(function () {
+                $('.li').children('span').removeClass('active-menu')
+                $(this).children('span').addClass('active-menu')
+            })
+
+
+        });
